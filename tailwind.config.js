631c268b2ddbf7e5a9d2b7bfa43dce055fa4e973
Patrick Orel <patrickorel18@gmail.com>
2022-08-26 21:13:45 -0500

    Added NavMenu, removed some unused pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    content: ["./**/*.{razor,html,cshtml}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'gugi': ['Gugi']
+            }
+        },
+    },
+    plugins: [],
 }
